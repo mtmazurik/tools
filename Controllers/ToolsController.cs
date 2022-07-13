cc83a8f88d7121cde8cf84adf62c85891e81d523
Marty Mazurik <marty@martymazurik.com>
2022-07-13 15:17:43 -0700

    initial

---
+++ Controllers/ToolsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tools.Controllers
+{
+    [ApiController]
+    public class ToolsController : Controller
+    {
+        [Route("/ping")]
+        [HttpGet]
+        public IActionResult GetPing()
+        {
+            return Ok();
+        }
+        [Route("/file")]
+        [HttpGet]
+        public IActionResult GetFile()
+        {
+            try
+            {
+                throw new FileNotFoundException("This is (NYI) not yet implemented.");
+            }
+            catch (FileNotFoundException exc)
+            {
+                var stdErr = Console.Error;
+
+                stdErr.WriteLine($"Failed file operation. Inner message : { exc.Message } ");
+                return NotFound();
+            }
+        }
+    }
+
+
+}
